bddd38ce55f63d0e17167e5979fb096bcbcb033b
HSteven1406 <steven.hanssens1406@gmail.com>
2021-04-07 13:14:59 +0200

    Typo

--- cachematrix.R
+++ cachematrix.R
@@ -7,7 +7,7 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
       x <<- y
-      m <<- null
+      inv <<- NULL
     }
     get <- function() x
     setinverse <- function(inverse) inv <<- inverse
